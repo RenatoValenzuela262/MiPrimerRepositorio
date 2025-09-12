089233e663d495364fc50d33634c6d583d13101b
Rena111 <revalenzue@gmail.com>
2025-09-12 00:44:31 -0300

    avance main

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,8 @@
 fun main(){
-    var pelicula1:Pelicula = Pelicula("Matrix","Accion","1 hora 30 min", mutableListOf(), "Wachowski", listOf("Keanu Reeves", "Carrie-Annie Moss"))
-    pelicula1.rellenarAsientos(pelicula1)
+    var asientosVacios:MutableList<String>
+
+    var pelicula1:Pelicula = Pelicula("Matrix","Accion","1 hora 30 min", asientosVacios, "Wachowski", listOf("Keanu Reeves", "Carrie-Annie Moss"))
+    pelicula1.rellenarAsientos(pelicula1.asientos)
+
+    println(pelicula1.asientos)
 }
